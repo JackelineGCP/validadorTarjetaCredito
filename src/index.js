50802acec8c3dab159539a07085a4ef2b876d734
kamalyzl <kamaly.kazal@gmail.com>
2018-01-27 17:03:10 -0500

    actualizando para realizar merge

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
-
-let inputCodeCard = '1234554222565851';
+// Solicitando al usuario su número de tarjeta de crédito
 const cl = console.log;
-let sum = 0;
+do {
+  var inputCodeCard = prompt('Ingrese su número de tarjeta de crédito \nNota: Recuerde que no puede ingresar espacios vacíos, letras o más de 16 dígitos');
+} while (!inputCodeCard || isNaN(inputCodeCard) || (inputCodeCard.length !== 16));
 
-// do {
-//   let inputCodeCard = prompt('Ingrese su número de tarjeta de crédito');   
-// } while (!inputCodeCard || isNaN(inputCodeCard) || (inputCodeCard.length !== 16));
-
-
-let isValidCard = (parametro) => {
-  let array = Array.from(inputCodeCard); // Convertirmos input en un array
-  let arrReverse = array.reverse(); // invertimos el array
-  // filtramos los indices pares para luego multiplicarlo por dos
-  arrReverse.filter((value, index, arr) => {
-    if (index % 2 === 0) {
-      let numberByTwo = value * 2;
-      let resultSumPar = numberByTwo.toString().split('').reduce((r, n) => {
-        return r + parseInt(n);
-      }, 0);
-
-      cl(typeof(resultSumPar));
-      // cl( xxx.concat(xxx));
-
-
+// Creando función para validar tarjeta
+function isValidCard(inputCodeCard) {
+  let arrReverse = []; // Creando un array donde se almacenarán los números de la tarjeta en orden inverso
+  for (let val of inputCodeCard) {
+    arrReverse.unshift(parseInt(val));
+    // cl(arrReverse);
+  }
+  // Obteniendo números en posición par
+  for (var j = 1; j < arrReverse.length; j = j + 2) {
+    if (arrReverse[j] * 2 >= 10) {
+      var numberByTwo = arrReverse[j] * 2;
+      // Si el valor de la multiplicación es de dos dígitos: sumar digitos y luego reemplzar en arrReverse
+      arrReverse[j] = parseInt(numberByTwo.toString(10)[0]) + parseInt(numberByTwo.toString(10)[1]);
+    } else {
+      // Si el valor de la multiplicación es de un dígito, se reemplaza en arrReverse
+      arrReverse[j] = arrReverse[j] * 2;
     }
-
-  });
-};
-// Validando tarjeta
-// sum % 10 === 0 ? cl('Tu número de tarjeta es VÁLIDA') : cl('Tu número de tarjeta es INVÁLIDA');
+    // Sumando los digitos de la tarjeta
+    var sum = 0;
+    for (let val of arrReverse) {
+      sum = sum + val;
+    }
+  }
+  // Validando tarjeta
+  if (sum % 10 === 0) {
+    return alert('Tu número de tarjeta es VÁLIDA');
+  } else {
+    return alert('Tu número de tarjeta es INVÁLIDA');
+  }
+}
 
 
 // Llamando a la función
