0aa2ca4c572a590e1f0e4a968a5e6517c68a7c1f
kamalyzl <kamaly.kazal@gmail.com>
2018-01-27 17:11:58 -0500

    Realizando nuevas pruebas

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-let saludar = name => `hola ${name}`;
-name('Kamaly');
+let saludo = (nombre, tratamiento) => {
+  alert('Hola ' + tratamiento + ' ' + nombre);
+};
+  
+// invocamos
+saludo('Andres', 'sr.');
